35b68d734da0124c6bff3ce06389dc594062fba5
winar-jin <winar1994@outlook.com>
2017-08-29 09:49:27 +0800

    complete of challenge 29

---
+++ 29 - Countdown Timer/scripts.js
@@ -0,0 +1,53 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds) {
+	// before start new timer, clear all exist timers
+	clearInterval(countdown);
+	const now = Date.now();
+	const then = now + seconds * 1000;
+	displayTimeLeft(seconds);
+	displayEndTime(then);
+	countdown = setInterval(() => {
+		const secondLeft = Math.round((then - Date.now()) / 1000);
+		// if the secondLeft is less than 0,wo shoule stop it
+		if (secondLeft < 0) {
+			clearInterval(countdown);
+			return;
+		}
+		// display the time
+		displayTimeLeft(secondLeft);
+	}, 1000);
+}
+
+function displayTimeLeft(seconds) {
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds<10?'0':''}${remainderSeconds}`;
+	timerDisplay.textContent = display;
+	document.title = display;
+}
+
+function displayEndTime(timestamp) {
+	const end = new Date(timestamp);
+	const hours = end.getHours();
+	const minutes = end.getMinutes();
+	const adjustHours = hours > 12 ? hours - 12 : hours;
+	endTime.textContent = `Be back At ${adjustHours}:${minutes<10?'0':''}${minutes}`;
+}
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time, 10);
+	timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer, false));
+
+document.customForm.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const seconds = parseInt(this.minutes.value, 10);
+	timer(seconds);
+	this.reset();
+}, false);
